9baa79de8a01179fc7ecea120a37fbde6b71a9ec
merqsous <moatherqsous@gmail.com>
2026-07-11 20:36:56 -0400

    Set no-cache headers to bust Railway CDN cache

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=3600, must-revalidate',
+            value: 'no-cache, no-store, must-revalidate',
           },
         ],
       },
